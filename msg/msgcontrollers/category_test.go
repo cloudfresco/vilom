bfe94014dc4bf5b3bc0309a80ebb0014bed09730
rl26 <4592388+rl26@users.noreply.github.com>
2019-08-05 14:12:59 +0530

    add category controller tests

---
+++ msg/msgcontrollers/category_test.go
@@ -0,0 +1,383 @@
+package msgcontrollers
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/throttled/throttled/store/goredisstore"
+
+	"github.com/cloudfresco/vilom/msg/msgservices"
+	"github.com/cloudfresco/vilom/testhelpers"
+	"github.com/cloudfresco/vilom/user/userservices"
+)
+
+func TestGetCategories(t *testing.T) {
+	var err error
+	err = testhelpers.LoadSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+
+	catService := msgservices.NewCategoryService(dbService, redisService)
+
+	topicService := msgservices.NewTopicService(dbService, redisService)
+	msgService := msgservices.NewMessageService(dbService, redisService)
+	userService := userservices.NewUserService(dbService, redisService, mailerService, jwtOpt, userOpt)
+	store, err := goredisstore.New(redisService.RedisClient, "throttled:")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	mux := http.NewServeMux()
+	Init(catService, topicService, msgService, userService, rateOpt, jwtOpt, mux, store)
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/v0.1/categories", bytes.NewBuffer([]byte(`{"limit": 20}, "cursor": ""`)))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	req.Header.Set("Authorization", "Bearer "+Tokenstring)
+
+	mux.ServeHTTP(w, req)
+
+	resp := w.Result()
+	// Check the status code is what we expect.
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Unexpected status code %d", resp.StatusCode)
+	}
+	expected := string(`{"Categories":[{"id":1,"id_s":"1bd1888a-dbfe-4510-a7ad-a98f69fd0a6b","category_name":"Performance Portable Transmitter","category_desc":"Performance Portable Transmitter","num_chd":1,"user_id":1,"statusc":1,"created_at":"2019-07-23T10:04:26Z","updated_at":"2019-07-23T10:04:26Z","created_day":204,"created_week":30,"created_month":7,"created_year":2019,"updated_day":204,"updated_week":30,"updated_month":7,"updated_year":2019,"Topics":null}],"next_cursor":"MA=="}` + "\n")
+
+	if w.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			w.Body.String(), expected)
+	}
+	err = testhelpers.DeleteSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestGetCategoryWithTopics(t *testing.T) {
+	var err error
+	err = testhelpers.LoadSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+
+	catService := msgservices.NewCategoryService(dbService, redisService)
+
+	topicService := msgservices.NewTopicService(dbService, redisService)
+	msgService := msgservices.NewMessageService(dbService, redisService)
+	userService := userservices.NewUserService(dbService, redisService, mailerService, jwtOpt, userOpt)
+	store, err := goredisstore.New(redisService.RedisClient, "throttled:")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	mux := http.NewServeMux()
+	Init(catService, topicService, msgService, userService, rateOpt, jwtOpt, mux, store)
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/v0.1/categories/1c29bf3a-4684-499c-a519-2c348aa13246", nil)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+Tokenstring)
+	mux.ServeHTTP(w, req)
+
+	resp := w.Result()
+	resp.StatusCode = 200
+	// Check the status code is what we expect.
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Unexpected status code %d", resp.StatusCode)
+	}
+	expected := string(`{"id":2,"id_s":"1c29bf3a-4684-499c-a519-2c348aa13246","category_name":"Drive","category_desc":"Drive","num_topics":1,"levelc":1,"parent_id":1,"user_id":1,"statusc":1,"created_at":"2019-07-23T10:04:26Z","updated_at":"2019-07-23T10:04:26Z","created_day":204,"created_week":30,"created_month":7,"created_year":2019,"updated_day":204,"updated_week":30,"updated_month":7,"updated_year":2019,"Topics":[{"id":1,"id_s":"44b2e674-7031-4487-be96-60093bfe8ac3","topic_name":"Floptical Question","topic_desc":"Floptical Question","num_messages":1,"category_id":2,"user_id":1,"statusc":1,"created_at":"2019-07-23T10:04:26Z","updated_at":"2019-07-23T10:04:26Z","created_day":204,"created_week":30,"created_month":7,"created_year":2019,"updated_day":204,"updated_week":30,"updated_month":7,"updated_year":2019,"Messages":null}]}` + "\n")
+	if w.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			w.Body.String(), expected)
+	}
+	err = testhelpers.DeleteSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestGetTopLevelCategories(t *testing.T) {
+	var err error
+	err = testhelpers.LoadSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+
+	catService := msgservices.NewCategoryService(dbService, redisService)
+
+	topicService := msgservices.NewTopicService(dbService, redisService)
+	msgService := msgservices.NewMessageService(dbService, redisService)
+	userService := userservices.NewUserService(dbService, redisService, mailerService, jwtOpt, userOpt)
+	store, err := goredisstore.New(redisService.RedisClient, "throttled:")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	mux := http.NewServeMux()
+	Init(catService, topicService, msgService, userService, rateOpt, jwtOpt, mux, store)
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/v0.1/categories/topcats", nil)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+Tokenstring)
+	mux.ServeHTTP(w, req)
+
+	resp := w.Result()
+	resp.StatusCode = 200
+	// Check the status code is what we expect.
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Unexpected status code %d", resp.StatusCode)
+	}
+
+	expected := string(`[{"id":1,"id_s":"1bd1888a-dbfe-4510-a7ad-a98f69fd0a6b","category_name":"Performance Portable Transmitter","category_desc":"Performance Portable Transmitter","num_chd":1,"user_id":1,"statusc":1,"created_at":"2019-07-23T10:04:26Z","updated_at":"2019-07-23T10:04:26Z","created_day":204,"created_week":30,"created_month":7,"created_year":2019,"updated_day":204,"updated_week":30,"updated_month":7,"updated_year":2019,"Topics":null}]` + "\n")
+
+	if w.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			w.Body.String(), expected)
+	}
+	err = testhelpers.DeleteSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestGetChildCategories(t *testing.T) {
+	var err error
+	err = testhelpers.LoadSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+
+	catService := msgservices.NewCategoryService(dbService, redisService)
+
+	topicService := msgservices.NewTopicService(dbService, redisService)
+	msgService := msgservices.NewMessageService(dbService, redisService)
+	userService := userservices.NewUserService(dbService, redisService, mailerService, jwtOpt, userOpt)
+	store, err := goredisstore.New(redisService.RedisClient, "throttled:")
+	if err != nil {
+		t.Error(err)
+		return
+
+	}
+	mux := http.NewServeMux()
+	Init(catService, topicService, msgService, userService, rateOpt, jwtOpt, mux, store)
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/v0.1/categories/1bd1888a-dbfe-4510-a7ad-a98f69fd0a6b/chdn", nil)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+Tokenstring)
+	mux.ServeHTTP(w, req)
+
+	resp := w.Result()
+	resp.StatusCode = 200
+	// Check the status code is what we expect.
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Unexpected status code %d", resp.StatusCode)
+	}
+
+	expected := string(`[{"id":2,"id_s":"1c29bf3a-4684-499c-a519-2c348aa13246","category_name":"Drive","category_desc":"Drive","num_topics":1,"levelc":1,"parent_id":1,"user_id":1,"statusc":1,"created_at":"2019-07-23T10:04:26Z","updated_at":"2019-07-23T10:04:26Z","created_day":204,"created_week":30,"created_month":7,"created_year":2019,"updated_day":204,"updated_week":30,"updated_month":7,"updated_year":2019,"Topics":null}]` + "\n")
+
+	if w.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			w.Body.String(), expected)
+	}
+	err = testhelpers.DeleteSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestGetParentCategory(t *testing.T) {
+	var err error
+	err = testhelpers.LoadSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+
+	catService := msgservices.NewCategoryService(dbService, redisService)
+
+	topicService := msgservices.NewTopicService(dbService, redisService)
+	msgService := msgservices.NewMessageService(dbService, redisService)
+	userService := userservices.NewUserService(dbService, redisService, mailerService, jwtOpt, userOpt)
+	store, err := goredisstore.New(redisService.RedisClient, "throttled:")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	mux := http.NewServeMux()
+	Init(catService, topicService, msgService, userService, rateOpt, jwtOpt, mux, store)
+
+	req, err := http.NewRequest("GET", "http://localhost:8000/v0.1/categories/1c29bf3a-4684-499c-a519-2c348aa13246/getparent", nil)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+Tokenstring)
+	mux.ServeHTTP(w, req)
+
+	resp := w.Result()
+	resp.StatusCode = 200
+	// Check the status code is what we expect.
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Unexpected status code %d", resp.StatusCode)
+	}
+
+	expected := string(`{"id":1,"id_s":"1bd1888a-dbfe-4510-a7ad-a98f69fd0a6b","category_name":"Performance Portable Transmitter","category_desc":"Performance Portable Transmitter","num_chd":1,"user_id":1,"statusc":1,"created_at":"2019-07-23T10:04:26Z","updated_at":"2019-07-23T10:04:26Z","created_day":204,"created_week":30,"created_month":7,"created_year":2019,"updated_day":204,"updated_week":30,"updated_month":7,"updated_year":2019,"Topics":null}` + "\n")
+
+	if w.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			w.Body.String(), expected)
+	}
+	err = testhelpers.DeleteSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestUpdateCategory(t *testing.T) {
+	var err error
+	err = testhelpers.LoadSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+
+	catService := msgservices.NewCategoryService(dbService, redisService)
+
+	topicService := msgservices.NewTopicService(dbService, redisService)
+	msgService := msgservices.NewMessageService(dbService, redisService)
+	userService := userservices.NewUserService(dbService, redisService, mailerService, jwtOpt, userOpt)
+	store, err := goredisstore.New(redisService.RedisClient, "throttled:")
+	if err != nil {
+		t.Error(err)
+	}
+	mux := http.NewServeMux()
+	Init(catService, topicService, msgService, userService, rateOpt, jwtOpt, mux, store)
+
+	data := []byte(`{"category_name" : "cat3", "category_desc" : "cat3 description"}`)
+
+	req, err := http.NewRequest("PUT", "http://localhost:8000/v0.1/categories/1c29bf3a-4684-499c-a519-2c348aa13246", bytes.NewBuffer(data))
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+Tokenstring)
+	mux.ServeHTTP(w, req)
+
+	resp := w.Result()
+	resp.StatusCode = 200
+	// Check the status code is what we expect.
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Unexpected status code %d", resp.StatusCode)
+	}
+
+	expected := string(`"Updated Successfully"` + "\n")
+
+	if w.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			w.Body.String(), expected)
+	}
+	err = testhelpers.DeleteSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestDeleteCategory(t *testing.T) {
+	var err error
+	err = testhelpers.LoadSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+
+	catService := msgservices.NewCategoryService(dbService, redisService)
+
+	topicService := msgservices.NewTopicService(dbService, redisService)
+	msgService := msgservices.NewMessageService(dbService, redisService)
+	userService := userservices.NewUserService(dbService, redisService, mailerService, jwtOpt, userOpt)
+	store, err := goredisstore.New(redisService.RedisClient, "throttled:")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	mux := http.NewServeMux()
+	Init(catService, topicService, msgService, userService, rateOpt, jwtOpt, mux, store)
+
+	req, err := http.NewRequest("DELETE", "http://localhost:8000/v0.1/categories/1c29bf3a-4684-499c-a519-2c348aa13246", nil)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+Tokenstring)
+	mux.ServeHTTP(w, req)
+
+	resp := w.Result()
+	resp.StatusCode = 200
+	// Check the status code is what we expect.
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Unexpected status code %d", resp.StatusCode)
+	}
+
+	expected := string(`"Deleted Successfully"` + "\n")
+
+	if w.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			w.Body.String(), expected)
+	}
+	err = testhelpers.DeleteSQL(dbService)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
---
+++ msg/msgcontrollers/msgcontrollers_test.go
@@ -0,0 +1,83 @@
+package msgcontrollers
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/cloudfresco/vilom/common"
+	"github.com/cloudfresco/vilom/testhelpers"
+	"github.com/cloudfresco/vilom/user/usercontrollers"
+	"github.com/cloudfresco/vilom/user/userservices"
+
+	"github.com/throttled/throttled/store/goredisstore"
+)
+
+var dbService *common.DBService
+var redisService *common.RedisService
+var mailerService *common.MailerService
+var serverOpt *common.ServerOptions
+var rateOpt *common.RateOptions
+var jwtOpt *common.JWTOptions
+var userOpt *common.UserOptions
+var Layout string
+var Tokenstring string
+
+func TestMain(m *testing.M) {
+	var err error
+
+	dbService, redisService, serverOpt, rateOpt, jwtOpt, _, userOpt, err = testhelpers.InitTestController()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	Layout = "2006-01-02T15:04:05Z"
+
+	err = testhelpers.LoadSQL(dbService)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	w := httptest.NewRecorder()
+
+	userService := userservices.NewUserService(dbService, redisService, mailerService, jwtOpt, userOpt)
+	ugroupService := userservices.NewUgroupService(dbService, redisService)
+	ubadgeService := userservices.NewUbadgeService(dbService, redisService)
+
+	store, err := goredisstore.New(redisService.RedisClient, "throttled:")
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	mux := http.NewServeMux()
+	usercontrollers.Init(userService, ugroupService, ubadgeService, rateOpt, jwtOpt, mux, store)
+
+	req, err := http.NewRequest("POST", "http://localhost:8000/v0.1/u/login", bytes.NewBuffer([]byte(`{"Email": "abcd145@gmail.com", "Password": "abc1238"}`)))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	mux.ServeHTTP(w, req)
+
+	user := userservices.User{}
+	dec := json.NewDecoder(strings.NewReader(w.Body.String()))
+	err = dec.Decode(&user)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	Tokenstring = user.Tokenstring
+	err = testhelpers.DeleteSQL(dbService)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	os.Exit(m.Run())
+}
